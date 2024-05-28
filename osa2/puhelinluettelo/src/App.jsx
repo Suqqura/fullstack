0645494fab8dce75d3dba06cbfbb9fed323b7d74
Henna Helin <henna.helin@edu.turkuamk.fi>
2024-05-28 21:46:03 +0300

    puhelinluettelo 2.9* - 2.10 tehty

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,23 +1,69 @@
-// puhelinluettelo, 2.6 - 2.8 done
+// puhelinluettelo, 2.6 - 2.10 done
 
 // imports
 import { useState } from 'react'
 
 
+// Filter
+const Filter = ({ searchName, handleSearchChange }) => {
+  return (
+    <div>
+      filter shown with <input value={searchName} onChange={handleSearchChange} />
+    </div>
+  )
+}
+
+
+// PersonForm
+const PersonForm = ({ addPerson, newName, newNumber, handleNameChange, handleNumberChange }) => {
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input 
+          value={newName} 
+          onChange={handleNameChange} 
+        />
+      </div>
+      <div>
+        number: <input
+          value={newNumber}
+          onChange={handleNumberChange}
+        />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+
+// Person
+const Persons = ({ persons }) => {
+  return (
+    <div>
+      {persons.map(person => (
+        <div key={person.name}>{person.name} {person.number}</div>
+      ))}
+    </div>
+  )
+}
+
+
 // App
 const App = () => {
-  // Data 2.8
+  // Data 2.9
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456' },
     { name: 'Ada Lovelace', number: '39-44-5323523' },
     { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
   ])
-
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchName, setSearchName] = useState('')
 
-
+  // event handlers
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
@@ -28,7 +74,12 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearchChange = (event) => {
+    console.log(event.target.value)
+    setSearchName(event.target.value)
+  }
 
+  // Add new person
   const addPerson = (event) => {
     event.preventDefault()
 
@@ -46,38 +97,36 @@ const App = () => {
     }
   }
 
+  // Filter / Search function
+  const personsToShow = searchName
+    ? persons.filter(person => person.name.toLowerCase().includes(searchName.toLowerCase()))
+    : persons
+
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input 
-            value={newName} 
-            onChange={handleNameChange} 
-          />
-        </div>
-        <div>
-          number: <input
-            value={newNumber}
-            onChange={handleNumberChange}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <div>
-        {persons.map(person => (
-          <div key={person.name}>{person.name} {person.number}</div>
-        ))}
-      </div>
+
+      <Filter searchName={searchName} handleSearchChange={handleSearchChange} />
+
+      <h3>add a new</h3>
+
+      <PersonForm 
+        newName={newName} 
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        addPerson={addPerson}
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons persons={personsToShow} />
     </div>
   )
 }
 
-// return ( <div>debug: {newName}</div> )
+// #testing# return ( <div>debug: {newName}</div> )
 
 
 // dont delete
